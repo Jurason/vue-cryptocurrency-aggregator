deb1e4c0e84d3eeb7487aa3398ee68ad89f7a323
Eugene <evgenilymanyuk@gmail.com>
2022-10-07 18:15:51 +0300

    initial

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+			{{ "Hello from Counter Component" }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CounterComponent',
+	data(){
+		return{
+			msg: ''
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
